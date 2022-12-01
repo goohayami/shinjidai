58290869d28927f766586d28285ae5d61566faa3
goohayami <goohayami@gmail.com>
2022-12-01 19:25:13 +0900

    新時代

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">新時代1</router-link>
+    <!-- <router-link to="/shintwo">新時代2</router-link> -->
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
