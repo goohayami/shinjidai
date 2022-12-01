11ce7e47c7723ec97c122988a79b6d495f8a5ca8
goohayami <98527002+goohayami@users.noreply.github.com>
2022-12-01 20:09:42 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
-import ShinTwo from "../views/Shintwo.vue";
+import ShinTwo from "../views/ShinTwo.vue";
 
 const routes = [
   {
